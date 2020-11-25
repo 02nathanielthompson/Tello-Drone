29f069d92200d3189f94b6eecdf5ea0826903bed
02nathanielthompson <72573749+02nathanielthompson@users.noreply.github.com>
2020-11-25 17:20:19 -0500

    Add files via upload

---
+++ TelloTemplate.py
@@ -0,0 +1,87 @@
+# Started from Tello Template
+# This Python app is in the Public domain
+# Some parts from Tello3.py
+
+import threading, socket, sys, time, subprocess
+
+
+# GLOBAL VARIABLES DECLARED HERE....
+host = ''
+port = 9000
+locaddr = (host,port)
+tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
+
+
+
+# Creates a UDP socketd
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(locaddr)
+
+
+def recv():
+    count = 0
+    while True:
+        try:
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\n****Keep Eye on Drone****\n')
+            break
+
+
+def sendmsg(msg, sleep = 6):
+    print("Sending: " + msg)
+    msg = msg.encode(encoding="utf-8")
+    sock.sendto(msg, tello_address)
+    time.sleep(sleep)
+
+# recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+
+# CREATE FUNCTIONS HERE....
+
+# Square Function with a for loop
+def square():
+    for i in range(4):
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+
+# Series of flips
+def flip():
+    sendmsg('flip f')
+    sendmsg('flip l')
+
+
+print("\nMr. Lange")
+print("Program Name: Drone Flying School")
+print("Date: 11.6.2020")
+print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
+print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
+ready = input('\nAre you ready to take flight: ')
+
+
+try:
+    if ready.lower() == 'yes':
+        print("\nStarting Drone!\n")
+
+        sendmsg('command')
+        sendmsg('takeoff', 8)
+
+        # Calling Functions with some being commented out for testing purposes
+        # square()
+        flip()
+
+        sendmsg('land')
+
+        print('\nGreat Flight!!!')
+
+    else:
+        print('\nMake sure you check WIFI, surroundings, co-pilot is ready, re-run program\n')
+except KeyboardInterrupt:
+    sendmsg('emergency')
+
+breakr = True
+sock.close()
